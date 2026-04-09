b277f75cb2279efe8741d1e6f165e4515089bc62
Prince Joes Paul <princejoespaul@gmail.com>
2026-04-09 10:08:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import joblib
 import numpy as np
 from preprocess import preprocess
@@ -48,5 +47,4 @@ def health():
 
 if __name__ == "__main__":
     # app.run(debug=True,host="0.0.0.0",port=8000
-    port = int(os.environ.get('PORT',8000))
-    app.run(host='0.0.0.0',port=port)
+    app.run(debug=True,host='0.0.0.0',port=8000)
